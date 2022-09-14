456e76a5d33804406cbec1e0470276a70aeb3c4a
Andrea Vesterhus <andrea.vesterhus@bouvet.no>
2022-09-14 09:55:58 +0200

    Moved dev packages to devdependecies, documentation, ts-config, and added dev scripts

--- src/components/ticker.tsx
+++ src/components/ticker.tsx
@@ -1,41 +1,33 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 
 export interface TickerProps {
-    interval: number,
-    children : any
+  interval: number;
+  children: any;
 }
 
-export const Ticker: React.FC<TickerProps> = ({
-    interval,
-    children
-  }: TickerProps): React.ReactElement => {
+export const Ticker: React.FC<TickerProps> = ({ interval, children }: TickerProps): React.ReactElement => {
+  const [res, setResult] = useState(children());
 
-    const [res, setResult] = useState(children());
+  useInterval(() => {
+    setResult(children());
+  }, interval);
 
-    useInterval(() => {
-        setResult(children());
-      }, interval);
-
-  return (
-    <>
-    {res}
-    </>
-  );
+  return <>{res}</>;
 };
 
 function useInterval(callback: () => void, delay: number) {
-    const savedCallback = useRef(callback);
-  
-    useEffect(() => {
-      savedCallback.current = callback;
-    });
-  
-    useEffect(() => {
-      function tick() {
-        savedCallback.current();
-      }
-  
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }, [delay]);
-  }
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
