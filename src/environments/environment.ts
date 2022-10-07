8f16465ffdf90800026f3ae0ce039c8fff8aa82a
Lukasz Turchan <lukasz.turchan@bluware.com>
2022-10-07 21:57:14 +0200

    Backend connection
    Reverse proxy added to nginx to solve issue with CORS and
    dynamic backend address from environment variable.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   usingFlowifyServer: JSON.parse(process.env.REACT_APP_USING_FLOWIFY_SERVER ?? 'true') as boolean,
-  baseUri: process.env.REACT_APP_BASE_URI ?? '/flowify-server/',
+  baseUri: process.env.REACT_APP_BASE_URI ?? '',
 };
